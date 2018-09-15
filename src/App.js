e3e864c151cadc91b87dc50140debabd7b7d7250
duke.du <duke@MacBook-Pro-2.local>
2018-09-15 16:50:07 +0800

    1. 部署了新的合约，原有的数据已经清除 2. 提供了getMessageCount方法，已在console中打印，可以根据需求使用

--- src/App.js
+++ src/App.js
@@ -1,16 +1,80 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
 // animate
-import { StyleSheet, css } from 'aphrodite';
-import { spaceInLeft, spaceOutRight } from 'react-magic';
+import {StyleSheet, css} from 'aphrodite';
+import {spaceInLeft, spaceOutRight} from 'react-magic';
 
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
-const abi = [ { "constant": false, "inputs": [ { "name": "s", "type": "string" }, { "name": "t", "type": "string" } ], "name": "setWord", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "random", "type": "uint256" } ], "name": "getRandomWord", "outputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "string" }, { "name": "", "type": "address" }, { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" } ];
+const abi = [
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "random",
+                "type": "uint256"
+            }
+        ],
+        "name": "getRandomWord",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "name": "",
+                "type": "string"
+            },
+            {
+                "name": "",
+                "type": "address"
+            },
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getMessageCount",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "s",
+                "type": "string"
+            },
+            {
+                "name": "t",
+                "type": "string"
+            }
+        ],
+        "name": "setWord",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
 let mycontract;
 
 
@@ -26,21 +90,22 @@ const styles = StyleSheet.create({
     }
 });
 
-const contractAddress = "0xb3e2957f9aa802a6287ef067e91d76eada7d6322" // 合约地址
+const contractAddress = "0x95e22f0f683f42bfbba37746b300e41d3ff6d083" // 合约地址
 var simpleStorageInstance // 合约实例
 
 
 class App extends Component {
 
-	constructor(props) {
-		super(props)
-		this.state = {
+    constructor(props) {
+        super(props)
+        this.state = {
             word: null,
+            lastTenWords: [],
             from: null,
             timestamp: null,
-			random: 0,
-			count: 0,
-			input: '',
+            random: 0,
+            count: 0,
+            input: '',
             web3: null,
             emptyTip: "还没有留言，快来创建全世界第一条留言吧~",
             firstTimeLoad: true,
@@ -51,147 +116,155 @@ class App extends Component {
             animate: "",
             in: css(styles.in),
             out: css(styles.out)
-		}
-	}
+        }
+    }
 
-	componentWillMount() {
-		getWeb3
-		.then(results => {
-			this.setState({
-				web3: results.web3
-			})
-            //使用原生api构造合约实例
-            console.log('provider :', this.state.web3.currentProvider);
-            mycontract = new results.web3.eth.Contract(abi, contractAddress);
-            console.log('mycontract :', mycontract);
+    componentWillMount() {
+        getWeb3
+            .then(results => {
+                this.setState({
+                    web3: results.web3
+                })
+                //使用原生api构造合约实例
+                console.log('provider :', this.state.web3.currentProvider);
+                mycontract = new results.web3.eth.Contract(abi, contractAddress);
+                console.log('mycontract :', mycontract);
 
-			this.instantiateContract()
-		})
-		.catch(() => {
-			console.log('Error finding web3.')
-		})
-	}
+                this.instantiateContract()
+            })
+            .catch(() => {
+                console.log('Error finding web3.')
+            })
+    }
 
     // 循环从区块上随机读取留言
-	randerWord() {
+    randerWord() {
         const that = this
-		setInterval(() => {
-			let random_num = Math.random() * (this.state.count? this.state.count: 0)
-			this.setState({
-				random: parseInt(random_num)
-			})
-			// console.log("setInterval读取", this.state.random)
-			simpleStorageInstance.getRandomWord(this.state.random)
-			.then(result => {
-                // console.log("setInterval读取成功", result)
-                if(result[1]!=this.setState.word){
-                    this.setState({
-                        animate: this.state.out
-                    })
-                    setTimeout(() => {
-                        that.setState({
-                            count: result[0].c[0],
-                            word: result[1],
-                            from: result[2],
-                            timestamp: result[3],
-                            animate: this.state.in
+        setInterval(async () => {
+            let random_num = Math.random() * (this.state.count? this.state.count: 0)
+            this.setState({
+            	random: parseInt(random_num)
+            })
+            console.log("setInterval读取", this.state.random)
+
+            let messageCount = await mycontract.methods.getMessageCount().call();
+            console.log('message count :', messageCount);
+
+            simpleStorageInstance.getRandomWord(this.state.random)
+                .then(result => {
+                    // console.log("setInterval读取成功", result)
+                    if (result[1] != this.setState.word) {
+                        this.setState({
+                            animate: this.state.out
                         })
-                    }, 2000)
-                }
-			})
-		}, 2000)
-	}
+                        setTimeout(() => {
+                            that.setState({
+                                count: result[0].c[0],
+                                word: result[1],
+                                from: result[2],
+                                timestamp: result[3],
+                                animate: this.state.in
+                            })
+                        }, 2000)
+                    }
+                })
+        }, 2000)
+    }
 
-	instantiateContract() {
+    instantiateContract() {
         const that = this
-		const contract = require('truffle-contract')
-		const simpleStorage = contract(SimpleStorageContract)
-		simpleStorage.setProvider(this.state.web3.currentProvider)
+        const contract = require('truffle-contract')
+        const simpleStorage = contract(SimpleStorageContract)
+        simpleStorage.setProvider(this.state.web3.currentProvider)
 
-		// Get accounts.
-		this.state.web3.eth.getAccounts((error, accounts) => {
-			simpleStorage.at(contractAddress).then(instance => {
-				simpleStorageInstance = instance
-				//console.log("合约实例获取成功")
-			})
-			.then(result => {
-				return simpleStorageInstance.getRandomWord(this.state.random)
-			})
-			.then(result => {
-                //console.log("读取成功", result)
-                if(result[1]!=this.setState.word){
-                    this.setState({
-                        animate: this.state.out
-                    })
-                    setTimeout(() => {
-                        that.setState({
-                            count: result[0].c[0],
-                            word: result[1],
-                            from: result[2],
-                            timestamp: result[3],
-                            animate: this.state.in,
+        // Get accounts.
+        this.state.web3.eth.getAccounts((error, accounts) => {
+            simpleStorage.at(contractAddress).then(instance => {
+                simpleStorageInstance = instance
+                //console.log("合约实例获取成功")
+            })
+                .then(result => {
+                    return simpleStorageInstance.getRandomWord(this.state.random)
+                })
+                .then(result => {
+                    //console.log("读取成功", result)
+                    if (result[1] != this.setState.word) {
+                        this.setState({
+                            animate: this.state.out
+                        })
+                        setTimeout(() => {
+                            that.setState({
+                                count: result[0].c[0],
+                                word: result[1],
+                                from: result[2],
+                                timestamp: result[3],
+                                animate: this.state.in,
+                                firstTimeLoad: false
+                            })
+                        }, 2000)
+                    } else {
+                        this.setState({
                             firstTimeLoad: false
                         })
-                    }, 2000)
-                }else{
-                    this.setState({
-                        firstTimeLoad: false
-                    })
-                }
-				this.randerWord()
-			})
+                    }
+                    this.randerWord()
+                })
 
-		})
-	}
+        })
+    }
 
-  	render() {
-		return (
-			<div className="container">
-				
-				<main>
-					<div className="main-container">
-						<div className="showword">
+    render() {
+        return (
+            <div className="container">
+
+                <main>
+                    <div className="main-container">
+                        <div className="showword">
                             <div className={this.state.animate}>
                                 {
                                     (simpleStorageInstance && !this.state.firstTimeLoad)
-                                    ? <span className={this.state.animate}>{this.state.word || this.state.emptyTip}</span>
-                                    : <img src={require("../public/loading/loading-bubbles.svg")} width="64" height="64"/>
+                                        ? <span
+                                            className={this.state.animate}>{this.state.word || this.state.emptyTip}</span>
+                                        : <img src={require("../public/loading/loading-bubbles.svg")} width="64"
+                                               height="64"/>
                                 }
                             </div>
                             <p className={this.state.animate}>
-                                <span>{this.state.word? "来源："+this.state.from: ""}</span>
-                                <span>{this.state.word? "时间："+this.formatTime(this.state.timestamp): ""}</span>
+                                <span>{this.state.word ? "来源：" + this.state.from : ""}</span>
+                                <span>{this.state.word ? "时间：" + this.formatTime(this.state.timestamp) : ""}</span>
                             </p>
                         </div>
-						<div className="setword">
-							<input type="text" value={this.state.input} onChange={e => this.inputWord(e)}/>
-							<button onClick={() => this.setWord()}>写入</button>
-						</div>
-						
-					</div>
-				</main>
-				
-                <div className={this.state.loading? "loading show": "loading"}>
+                        <div className="setword">
+                            <input type="text" value={this.state.input} onChange={e => this.inputWord(e)}/>
+                            <button onClick={() => this.setWord()}>写入</button>
+                        </div>
+
+                    </div>
+                </main>
+
+                <div className={this.state.loading ? "loading show" : "loading"}>
                     <img src={require("../public/loading/loading-bubbles.svg")} width="128" height="128"/>
                     <p>Matemask 钱包确认支付后开始写入留言</p>
                     <p>{this.state.loadingTip}</p>
                 </div>
-			</div>
-		);
-	}
-	inputWord(e){
-		this.setState({
-			input: e.target.value
-		})
+            </div>
+        );
     }
+
+    inputWord(e) {
+        this.setState({
+            input: e.target.value
+        })
+    }
+
     // 写入区块链
-	async setWord(){
-        if(!this.state.input) return
+    async setWord() {
+        if (!this.state.input) return
         const that = this
         this.setState({
             loading: true
         })
-		let timestamp = new Date().getTime()
+        let timestamp = new Date().getTime()
         let accounts = await this.state.web3.eth.getAccounts();
         // let account0 = '0x22c57F0537414FD95b9f0f08f1E51d8b96F14029';
         const currentAccount = accounts[0];
@@ -202,7 +275,7 @@ class App extends Component {
             // let result = await simpleStorageInstance.setWord(this.state.input, String(timestamp), {from: currentAccount})
             let result = await mycontract.methods.setWord(this.state.input, String(timestamp)).send({
                 from: currentAccount,
-                gas : 3000000,
+                gas: 3000000,
             });
             console.log('write successfully!', result);
             alert(`transactionHash : , ${result.transactionHash}`);
@@ -226,7 +299,8 @@ class App extends Component {
             })
         }
 
-	}
+    }
+
     // 时间戳转义
     formatTime(timestamp) {
         let date = new Date(Number(timestamp))
@@ -236,9 +310,10 @@ class App extends Component {
         let hour = date.getHours()
         let minute = date.getMinutes()
         let second = date.getSeconds()
-        let fDate = [year, month, day, ].map(this.formatNumber)
-        return fDate[0] + '年' + fDate[1] + '月' + fDate[2] + '日' + ' ' + [hour, minute, second].map(this.formatNumber).join(':') 
+        let fDate = [year, month, day,].map(this.formatNumber)
+        return fDate[0] + '年' + fDate[1] + '月' + fDate[2] + '日' + ' ' + [hour, minute, second].map(this.formatNumber).join(':')
     }
+
     /** 小于10的数字前面加0 */
     formatNumber(n) {
         n = n.toString()
